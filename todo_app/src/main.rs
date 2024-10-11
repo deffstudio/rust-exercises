fbf337eafcc0364b71010ab3e7aedc2bd5906457
deffstudio <dudeyahuu@gmail.com>
2024-10-11 22:16:24 +0700

    Implemented to_do factory

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -1,12 +1,18 @@
 mod to_do;
-use to_do::structs::done::Done;
-use to_do::structs::pending::Pending;
+use to_do::enums::TaskStatus;
+use to_do::to_do_factory;
+use to_do::ItemTypes;
 
 fn main() {
-    let done = Done::new("Shopping");
-    println!("{}", done.super_struct.title);
-    println!("{}", done.super_struct.status.stringify());
-    let pending = Pending::new("Cook dinner");
-    println!("{}", pending.super_struct.title);
-    println!("{}", pending.super_struct.status.stringify());
+    let to_do_item = to_do_factory("Jogging", TaskStatus::DONE);
+    match to_do_item {
+        ItemTypes::Done(item) => {
+            println!("{}", item.super_struct.status.stringify());
+            println!("{}", item.super_struct.title);
+        }
+        ItemTypes::Pending(item) => {
+            println!("{}", item.super_struct.status.stringify());
+            println!("{}", item.super_struct.title);
+        }
+    }
 }
--- todo_app/src/to_do/mod.rs
+++ todo_app/src/to_do/mod.rs
@@ -1,2 +1,19 @@
 pub mod enums;
 pub mod structs;
+
+use enums::TaskStatus;
+use structs::done::Done;
+use structs::pending::Pending;
+
+pub enum ItemTypes {
+    Pending(Pending),
+    Done(Done),
+}
+
+// Factory function for to_do app
+pub fn to_do_factory(title: &str, status: TaskStatus) -> ItemTypes {
+    match status {
+        TaskStatus::DONE => ItemTypes::Done(Done::new(title)),
+        TaskStatus::PENDING => ItemTypes::Pending(Pending::new(title)),
+    }
+}
