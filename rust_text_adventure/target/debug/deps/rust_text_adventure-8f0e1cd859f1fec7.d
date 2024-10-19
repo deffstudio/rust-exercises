ce27d540bf780ad731846733976aed3815808ce3
sol_dude <141769472+deffstudio@users.noreply.github.com>
2024-10-19 10:09:29 +0700

    Delete rust_text_adventure directory

--- rust_text_adventure/target/debug/deps/rust_text_adventure-8f0e1cd859f1fec7.d
+++
@@ -1,5 +0,0 @@
-C:\Users\user\Desktop\feferdia\___codes\rust-exercises\rust_text_adventure\target\debug\deps\librust_text_adventure-8f0e1cd859f1fec7.rmeta: src/main.rs
-
-C:\Users\user\Desktop\feferdia\___codes\rust-exercises\rust_text_adventure\target\debug\deps\rust_text_adventure-8f0e1cd859f1fec7.d: src/main.rs
-
-src/main.rs:
--- rust_text_adventure/target/debug/deps/rust_text_adventure-bdfb22e1296971ee.d
+++
@@ -1,5 +0,0 @@
-C:\Users\user\Desktop\feferdia\___codes\rust-exercises\rust_text_adventure\target\debug\deps\librust_text_adventure-bdfb22e1296971ee.rmeta: src/main.rs
-
-C:\Users\user\Desktop\feferdia\___codes\rust-exercises\rust_text_adventure\target\debug\deps\rust_text_adventure-bdfb22e1296971ee.d: src/main.rs
-
-src/main.rs:
