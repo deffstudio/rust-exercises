5cc16822e535572633bba1606c9bd9d342882389
deffstudio <dudeyahuu@gmail.com>
2024-10-19 10:01:19 +0700

    Added initial rust_text_adventure project structure

---
+++ rust_text_adventure/target/debug/deps/rust_text_adventure-bdfb22e1296971ee.d
@@ -0,0 +1,5 @@
+C:\Users\user\Desktop\feferdia\___codes\rust-exercises\rust_text_adventure\target\debug\deps\librust_text_adventure-bdfb22e1296971ee.rmeta: src/main.rs
+
+C:\Users\user\Desktop\feferdia\___codes\rust-exercises\rust_text_adventure\target\debug\deps\rust_text_adventure-bdfb22e1296971ee.d: src/main.rs
+
+src/main.rs:
